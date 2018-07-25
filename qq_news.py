fe6ae46b8b8cc3f5d639454432c34232396149d5
lengyue1024 <xzhxpx@qq.com>
2018-07-25 15:18:52 +0800

    ’python小程序‘

---
+++ qq_news.py
@@ -0,0 +1,22 @@
+#!/bin/bash/python
+#coding=utf-8
+# 导入模块
+import prettytable as pt
+import requests
+from bs4 import BeautifulSoup
+url = "http://news.qq.com/"
+# 请求腾讯新闻的ＵＲＬ
+data = requests.get(url).text
+# 对请求的页面进行解析
+soup = BeautifulSoup(data,'lxml')
+# 从解析文件中通过select选择器定位到指定的元素，并返回一个列表
+news_titles = soup.select("div.text > em.f14 > a.linkto")
+# 对返回的列表进行遍历
+tb = pt.PrettyTable(["标题","链接"])
+for n in news_titles:
+	# 提取出标题和链接信息
+	title = n.get_text()
+	link = n.get("href")
+	tb.add_row([title,link])
+print(tb)
+
